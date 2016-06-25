850ec49da8113fdbe7beed720ed0c95724a88348
saintaxl <3338319@qq.com>
2016-06-25 23:42:46 +0800

    add select all

---
+++ cloud/src/main/java/com/mycloud/store/controller/rest/business/ExpressRestController.java
@@ -0,0 +1,12 @@
+package com.mycloud.store.controller.rest.business;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycloud.store.controller.BaseController;
+
+@RestController
+public class ExpressRestController extends BaseController {
+	
+	
+
+}
